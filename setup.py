578ea95522252d0713aa447dd68afa3c47ab3876
Raymond Luo <luolinhao1998@gmail.com>
2020-07-04 09:34:45 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*-coding:utf-8 -*-
+#Reference:**********************************************
+# @Time     : 2020/7/4 9:30
+# @Author   : Raymond Luo
+# @File     : setup.py.py
+# @User     : luoli
+# @Software: PyCharm
+#Reference:**********************************************
+import setuptools
+
+setuptools.setup(
+    name="Code-similarity", # Replace with your own username
+    version="0.0.1",
+    author="Raymond Luo",
+    author_email="luolinhao1998@gmail.com",
+    description="An out of box code and text similarity computation pacakage",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+)
