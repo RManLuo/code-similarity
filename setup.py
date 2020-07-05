cc6b19b27ae76e283dab5f09836de3447bedecfc
Raymond Luo <luolinhao1998@gmail.com>
2020-07-05 09:11:07 +0800

    Update version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="code-similarity",  # Replace with your own username
-    version="0.0.3",
+    version="0.0.4",
     author="Raymond Luo",
     author_email="luolinhao1998@gmail.com",
     description="An out of box code and text similarity computation package",
